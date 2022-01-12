1a630f3d747ed3d6ebff5bf8f3d0e1fd01e547bb
Szampan <szampan@gmail.com>
2022-01-12 13:34:11 +0100

    Add controlling with parameters functionality

--- ScrapSkills.py
+++ ScrapSkills.py
@@ -18,6 +18,7 @@ Done:
 from requests import get
 from bs4 import BeautifulSoup
 from collections import Counter
+from dataclasses import dataclass
 from matplotlib import pyplot as plt
 from icecream import ic
 # import pandas as pd
@@ -26,40 +27,39 @@ import time
 
 start_time = time.time()
 
-base_URL = "https://nofluffjobs.com"
 # URL = "https://nofluffjobs.com/pl/praca-it/praca-zdalna/python?criteria=city%3Dbialystok%20seniority%3Dtrainee,junior"
-URL = "https://nofluffjobs.com/pl/praca-it/python?criteria=seniority%3Dtrainee,junior"
-to_exclude = ['python']    
+URL = "https://nofluffjobs.com/pl/praca-it/python?criteria=seniority%3Dtrainee,junior"  # TODO = URL generated in the basis of search keywords
+
+@dataclass
+class Config:
+    base_URL: str = "https://nofluffjobs.com"
+    # URL: str = "None"                     # TODO: URL generated on the basis of search keywords
+    keywords: str = "C flat"                # str or list?
+    to_exclude: tuple = ("python",)           
+    obligatory_print_num: int = 20
+    obligatory_plot_num: int = 15           
+    nice_to_have_print_num: int = 15        
+    subpages_parse_num: int = None             # For testing. None if parse all
+    
 
 def main():
-    page = get(URL)
-    URL_subpages = get_list_of_subpages(URL) # TODO: add next page functionality
+    URL_subpages = get_list_of_subpages(URL)
     obligatory = []
     nice_to_have = []
-    
-    # try:
-
-    obligatory, nice_go_have = get_skills_from_subpages(URL_subpages[:3])
-    obligatory = exclude_unnecessary(obligatory, to_exclude)
-    ic(obligatory)
+    obligatory, nice_to_have = get_skills_from_subpages(URL_subpages[:Config.subpages_parse_num]) 
+    obligatory = exclude_unnecessary(obligatory, Config.to_exclude)
 
     print('\nOBLIGATORY:')
-    print_most_common(obligatory, 15)
+    print_most_common(obligatory, Config.obligatory_print_num)
     print('\nNICE TO HAVE:')
-    print_most_common(nice_to_have, 10)     # what if there is less than 10? fix it
+    print_most_common(nice_to_have, Config.nice_to_have_print_num)     # what if there is less than 10? fix it
 
     plot_most_common(obligatory, 15, f"Most commonly requested skills ({len(URL_subpages)} offers)")
     # plot_most_common(nice_to_have, 15, "Nice to have skills")
 
-
-    # except: 
-    #     print('No subpages found')
-
-
 def get_list_of_subpages(url) -> list:     # ADD FILTER ('python')
     print('\nGET LIST OF SUBPAGES')
     subpages = [] 
-
     while True:
         page = get(url)
         bs = BeautifulSoup(page.content, 'html.parser')
@@ -67,7 +67,6 @@ def get_list_of_subpages(url) -> list:     # ADD FILTER ('python')
         for offer in offers:
             href = offer['href']
             subpages.append(href)
-
         url = get_next_page_url(bs)
         if not url:
             break
@@ -78,22 +77,24 @@ def get_next_page_url(bs):
     next = bs.body.find('a', attrs={'aria-label': 'Next'})
     if next:
         ic(next['href'])
-        return base_URL + next['href']
+        return Config.base_URL + next['href']
     print('NO NEXT PAGE')
     return
 
 def get_skills_from_subpages(url_subpages) -> tuple: 
+    # test_obligatory = ['lulz', 'kicks', 'phun', 'kek', 'kek']       # for tests only
+    # test_nice_to_have = ['a', 'b', 'c']
+    # return (test_obligatory, test_nice_to_have)
     tmp_nice_to_have = []
     tmp_obligatory = []
     for url_subpage in url_subpages:
-        print('SUBPAGE:', base_URL + url_subpage)
-        subpage = get(base_URL + url_subpage)
+        print('SUBPAGE:', Config.base_URL + url_subpage)
+        subpage = get(Config.base_URL + url_subpage)
         skills = find_skills_on_subpage(subpage)
         tmp_obligatory += skills[1]
         tmp_nice_to_have += skills[0]
     
-    ic(tmp_obligatory, tmp_nice_to_have)
-    return(tmp_obligatory, tmp_nice_to_have)
+    return (tmp_obligatory, tmp_nice_to_have)
 
 def find_skills_on_subpage(subpage):
     bs = BeautifulSoup(subpage.content, 'html.parser')
@@ -133,7 +134,7 @@ def plot_most_common(list_of_items, number, title):
     # plt.xlabel("Popularity")
 
     # colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99']
-    font = {'family': 'normal',
+    font = {#'family': 'normal',
             #'weight' : 'bold',
             'size': 8}
     plt.rc('font', **font)
@@ -142,9 +143,14 @@ def plot_most_common(list_of_items, number, title):
     plt.show()
 
 def exclude_unnecessary(lst, to_exclude):
+    ic(to_exclude)
+    ic(lst)
+
     if to_exclude:
         for word in to_exclude:
-            return list(filter(lambda x: x.lower().strip() != word.lower(), lst))
+            ic(word)
+            lst = list(filter(lambda x: x.lower().strip() != word.lower(), lst))
+    ic(lst)
     return lst
 
 
