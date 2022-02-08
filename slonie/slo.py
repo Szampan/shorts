6f15f6649f5fcdcd987c968c63972de2c310eb41
Szampan <szampan@gmail.com>
2022-02-09 00:47:39 +0100

    Slonie: while loop instead of recursion

--- slonie/slo.py
+++ slonie/slo.py
@@ -1,20 +1,16 @@
 import sys
 from icecream import ic
+import time
+start_time = time.time()
+
 input_data = [list(map(int, i.split())) for i in sys.stdin]
 n, masses, initial_order, target_order = input_data
 n = n[0]
 
 shift_graph = {elephant: initial_order[target_order.index(elephant)] for elephant in initial_order}
-ic(shift_graph)
 cycles = {} 
 visited = [False] * n
-cycle_number = 0   
-
-def mark_vertex_visited(elephant):
-    if not visited[elephant-1]:
-        visited[elephant-1] = True
-        cycles[cycle_number].append(elephant)
-        mark_vertex_visited(shift_graph[elephant])
+cycle_number = 0  
 
 def get_mass(elephant):
     return masses[elephant-1]
@@ -22,9 +18,14 @@ def get_mass(elephant):
 for i in range(n):
     elephant = i + 1 
     if not visited[i]:
-        cycle_number += 1
         cycles[cycle_number] = []
-        mark_vertex_visited(elephant)
+        while not visited[elephant-1]:
+            cycles[cycle_number].append(elephant)
+            visited[elephant-1] = True
+            elephant = shift_graph[elephant]
+        cycle_number += 1
+
+print("---AFTER CYCLES--- %s seconds ---" % (time.time() - start_time))
 
 min_mass_global = 6500
 total_masses_per_cycle = []
@@ -44,9 +45,10 @@ for cycle in cycles:
 
 result = 0
 for i in range(cycle_number):
-    method_1 = total_masses_per_cycle[i] + (len(cycles[i+1]) - 2) * min_masses_per_cycle[i]
-    method_2 = total_masses_per_cycle[i] + min_masses_per_cycle[i] + (len(cycles[i+1]) + 1) * min_mass_global
+    method_1 = total_masses_per_cycle[i] + (len(cycles[i+0]) - 2) * min_masses_per_cycle[i]          
+    method_2 = total_masses_per_cycle[i] + min_masses_per_cycle[i] + (len(cycles[i+0]) + 1) * min_mass_global
     result += min(method_1, method_2)
 
 print(result)
 
+print("---FINISH--- %s seconds ---" % (time.time() - start_time))
